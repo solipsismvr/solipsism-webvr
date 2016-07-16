4987b54bb61e70530f4aa3872be2c20c0216222d
Sam Minnee <sam@silverstripe.com>
2016-07-16 18:00:23 +1200

    Fix repeat-loading of vive controller.

--- src/ViveControllerLoader.js
+++ src/ViveControllerLoader.js
@@ -3,14 +3,26 @@ var ObjLoader = require('./support/ObjLoader');
 function ViveControllerLoader(THREE) {
   var viveController;
 
+  var callbacks = [];
+
   return function (properties, callback) {
+
+    // It has loaded
     if(viveController) {
       callback(viveController.clone());
 
+    // It's in the middle of loading
+    } else if(callbacks.length) {
+      callbacks.push(callback);
+
+    // It hasn't started loading
     } else {
       var vivePath = 'models/';
       var loader = new ObjLoader(THREE);
       var self = this;
+
+      callbacks = [callback];
+
       loader.load(vivePath + '/vr_controller_vive_1_5.obj', function (object) {
         var loader = new THREE.TextureLoader();
 
@@ -20,7 +32,10 @@ function ViveControllerLoader(THREE) {
 
         viveController = object;
 
-        callback(viveController.clone());
+        callbacks.forEach(function (callback) {
+          callback(viveController.clone());
+        });
+        callbacks = [];
       });
     }
   }
